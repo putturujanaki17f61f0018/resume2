271747f70e5c8fb0663fe2ae48d36915802f076b
putturujanaki17f61f0018 <putturujanaki@gmail.com>
2018-08-01 12:10:58 +0530

    education updated

---
+++ main.js
@@ -0,0 +1,51 @@
+function loadJSON(file)
+{
+  return new Promise((resolve,reject)=>{
+  return fetch(file).then(response=>{
+    if(response.ok){
+      resolve(response.json());
+    }
+    else{
+      reject(new Error('error'));
+    }
+  })
+  })
+}
+var fetchedData=loadJSON("data.json");
+fetchedData.then(data=>{
+  console.log(data);
+  career(data.career);
+  education(data.education);
+  })
+  var child2=document.querySelector("#child2");
+  // career
+  function career(car){
+    var heading=document.createElement("h1");
+    heading.textContent="career Objective";
+    child2.appendChild(heading);
+    var hLine=document.createElement("hr");
+    heading.appendChild(hLine);
+    var p=document.createElement("p");
+
+    p.textContent=car.info;
+    child2.appendChild(p);
+  }
+  // Education
+function education(edu){
+  var heading=document.createElement("h2");
+  heading.textContent="Educational Qualification";
+  child2.appendChild(heading);
+  var hLine=document.createElement("hr");
+  heading.appendChild(hLine);
+  var table=document.createElement("table");
+  child2.appendChild(table);
+  var tr="<tr> <td>S.No</td><td> Degree</td><td> Institute</td><td> Data</td> </tr>";
+  // table.innerHTML=tr;
+  table.border="1";
+  var tr1="";
+  for(var i=0;i<edu.length;i++){
+    tr1+="<tr> <td>"+(i+1)+"</td><td>"+edu[i].degree+"</td><td>"+edu[i].institute+"</td><td>"+edu[i].data+"</td><td>";
+  }
+  table.innerHTML=tr+tr1;
+
+}
